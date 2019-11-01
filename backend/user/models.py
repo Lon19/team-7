3d79ddafc7693b83f541c8af9aaad184ea02a1ef
willdavitt <will.davitt@gmail.com>
2019-11-01 23:00:36 +0000

    Start of new database models
    Yay

--- backend/user/models.py
+++ backend/user/models.py
@@ -4,3 +4,16 @@ file = "../../Sample Data/wpforms-Autistica-8211-Work-Self-Confidence-10-29-2019
 
 
 class Questionnaire(models.Model):
+    questionnaireID = models.IntegerField
+    questionnaireName = models.CharField(max_length=200)
+
+class QuestionnaireQuestion(models.Model):
+    questionnaireID = models.ForeignKey(Questionnaire, on_delete=models.CASCADE) #check
+    questionID = models.IntegerField
+    questionType = models.IntegerField
+    questionText = models.CharField(max_length=200)
+
+class User(models.Model):
+    userID = models.IntegerField
+    gender = models.CharField(max_length)=20
+    age = models.Char
