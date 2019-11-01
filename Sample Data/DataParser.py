62b8739ea0d47645bff459d7a10f9603960d05d2
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2019-11-01 23:54:14 +0000

    Should work When Database Up #DarkFate #SaveMe

--- Sample Data/DataParser.py
+++ Sample Data/DataParser.py
@@ -1,11 +1,26 @@
 import csv
+<<<<<<< Updated upstream
+=======
+import json
+>>>>>>> Stashed changes
 
 class DataParser():
 
 
+<<<<<<< Updated upstream
     def __init__(self, fileName):
         self.responses = []
         self.questions = []
+=======
+    def __init__(self):
+        self.responses = []
+        self.questions = []
+        self.isUsers = False
+
+
+    def parseFile(self, fileName, isUsers):
+        self.isUsers = isUsers
+>>>>>>> Stashed changes
         with open(fileName,'rt')as f:
             data = csv.reader(f)
             count = 0
@@ -15,13 +30,44 @@ class DataParser():
                 else:
                     self.responses.append(row)
                 count += 1
+<<<<<<< Updated upstream
 
     def getQuestions(self):
         return self.questions
+=======
+                
+
+    def getQuestions(self):
+        questionResponse = []
+
+        if isUsers:
+            questionResponse.append(json.dumps({'FormName': self.questions[1]}))
+            questionResponse.append(json.dumps({'username': self.questions[2]}))
+            questionResponse.append(json.dumps({'iD': self.questions[-2]}))
+            questionResponse.append(json.dumps({'date': self.questions[-1]}))
+                                    
+            for i in range(3,len(self.questions)-2):        
+                questionJSON = {}
+                questionJSON['questionText'] = self.questions[i]
+                questionJSON['questionType'] = 1 # Hard coded for now
+                questionResponse.append(json.dumps(questionJSON))
+
+        else:
+            
+        
+        return questionResponse
+>>>>>>> Stashed changes
 
     def getResponses(self):
         return self.responses
 
 
+<<<<<<< Updated upstream
 data = DataParser("wpforms-Autistica-8211-Mental-Health.csv")
+=======
+data = DataParser()
+#data.parseFile("wpforms-Autistica-8211-Mental-Health.csv", False)
+data.parseFile("Participant-attribute-data.csv", True)
+
+>>>>>>> Stashed changes
 print(data.getQuestions())
