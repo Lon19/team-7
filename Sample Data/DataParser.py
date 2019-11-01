b2f049b5d6b3b8734071b8ba56e62d608ee2c20c
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2019-11-01 21:51:50 +0000

    Switched to OOP for Parser

--- Sample Data/DataParser.py
+++ Sample Data/DataParser.py
@@ -1,10 +1,27 @@
 import csv
-with open('wpforms-Autistica-8211-Adjustments-10-29-2019.csv','rt')as f:
-  data = csv.reader(f)
-  count = 0
-  for row in data:
-        if count == 0:
-              titles = row
-        #print(row)
-        count += 1
-print(titles)
+
+class DataParser():
+
+
+    def __init__(self, fileName):
+        self.responses = []
+        self.questions = []
+        with open(fileName,'rt')as f:
+            data = csv.reader(f)
+            count = 0
+            for row in data:
+                if count == 0:
+                    self.questions = row
+                else:
+                    self.responses.append(row)
+                count += 1
+
+    def getQuestions(self):
+        return self.questions
+
+    def getResponses(self):
+        return self.responses
+
+
+data = DataParser("wpforms-Autistica-8211-Mental-Health.csv")
+print(data.getQuestions())
