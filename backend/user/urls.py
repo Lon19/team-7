05e9f9988ae409adc3573c4aefa4fc64c4f67930
Andrew Morton <andrewmorton2@hotmail.co.uk>
2019-11-02 05:45:32 +0000

    Fixed various merge memes.

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -6,16 +6,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # index pages
-<<<<<<< HEAD
     path('respondent/home', views.user_home, name='user_home'),
     path('respondent/<int:user_id>/questionnaire/<int:questionnaire_id>', views.user_questionnaire, name='questionnaire'),
     path('respondent/<int:user_id>/visualisation/<int:questionnaire_id>', views.user_questionnaire_responses, name='questionnaire-responses'),
-    path('researcher/home', views.researcher_home, name="researcher_home")
-    path('researcher/<int:user_id>/visualisation/<int:questionnaire_id>')
-=======
-    path('home', views.home, name='home'),
-    path('questionnaire/<int:questionnaire_id>', views.questionnaire, name='questionnaire'),
+    path('researcher/home', views.researcher_home, name="researcher_home"),
+    path('researcher/<int:user_id>/visualisation/<int:questionnaire_id>', views.researcher_questionnaire_data, name="research_q_data"),
     path('users', views.getUsers, name='users'),
-    #path('visualisation/<int:questionnaire_id>', views.questionnaire_responses, name='questionnaire-responses'),
->>>>>>> aa67b9ba35e1095fd68a720141063845c5dad3d2
 ]
