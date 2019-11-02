7ea517618662b8503b6cbd46994e8c2616c9c62c
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2019-11-02 03:40:53 +0000

    Transfered DataParser to Branch #Mine

--- Sample Data/DataParser.py
+++ Sample Data/DataParser.py
@@ -1,10 +1,54 @@
 import csv
-with open('wpforms-Autistica-8211-Adjustments-10-29-2019.csv','rt')as f:
-  data = csv.reader(f)
-  count = 0
-  for row in data:
-        if count == 0:
-              titles = row
-        #print(row)
-        count += 1
-print(titles)
+import json
+
+class DataParser():
+
+
+    def __init__(self):
+        self.responses = []
+        self.questions = []
+        self.isUsers = False
+
+
+    def parseFile(self, fileName, isUsers):
+        self.isUsers = isUsers
+        with open(fileName,'rt')as f:
+            data = csv.reader(f)
+            count = 0
+            for row in data:
+                if count == 0:
+                    self.questions = row
+                else:
+                    self.responses.append(row)
+                count += 1
+
+
+    def getQuestions(self):
+        questionResponse = []
+
+        if self.isUsers:
+            questionResponse.append(json.dumps({'FormName': self.questions[1]}))
+            questionResponse.append(json.dumps({'username': self.questions[2]}))
+            questionResponse.append(json.dumps({'iD': self.questions[-2]}))
+            questionResponse.append(json.dumps({'date': self.questions[-1]}))
+
+            for i in range(3,len(self.questions)-2):
+                questionJSON = {}
+                questionJSON['questionText'] = self.questions[i]
+                questionJSON['questionType'] = 1 # Hard coded for now
+                questionResponse.append(json.dumps(questionJSON))
+
+        else:
+
+            pass
+        return questionResponse
+
+    def getResponses(self):
+        return self.responses
+
+
+data = DataParser()
+data.parseFile("wpforms-Autistica-8211-Mental-Health.csv", False)
+#data.parseFile("Participant-attribute-data.csv", True)
+
+print(data.getQuestions())
