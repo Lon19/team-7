6d9660e7bcfc2006bf7c18b276cdc93d352ebfdf
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2019-11-02 09:41:32 +0000

    Added Answers URL

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), # index pages
     path('users', views.getUsers, name='users'),
+    path('answers', views.getAnswers, name='answers'),
     path('respondent/home', views.user_home, name='user_home'),
     path('respondent/<int:user_id>/questionnaire/<int:questionnaire_id>', views.user_questionnaire, name='questionnaire'),
     path('respondent/<int:user_id>/visualisation/<int:questionnaire_id>', views.user_questionnaire_responses, name='questionnaire-responses'),
