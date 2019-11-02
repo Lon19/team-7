5f218009754eb00b4b98cf813639460f6edf13b7
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2019-11-02 06:06:07 +0000

    Fixed logic error - getResponses now gives responses #Oops

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('respondent/home', views.user_home, name='user_home'),
     path('respondent/<int:user_id>/questionnaire/<int:questionnaire_id>', views.user_questionnaire, name='questionnaire'),
     path('respondent/<int:user_id>/visualisation/<int:questionnaire_id>', views.user_questionnaire_responses, name='questionnaire-responses'),
-    path('researcher/home', views.researcher_home, name="researcher_home")
-    path('researcher/<int:user_id>/visualisation/<int:questionnaire_id>')
+    path('researcher/home', views.researcher_home, name="researcher_home"),
+    path('researcher/<int:user_id>/visualisation/<int:questionnaire_id>', views.researcher_questionnaire_data, name='research_q_data'),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -21,19 +21,18 @@ def user_questionnaire_responses(request, user_id, questionnaire_id): #request, 
 def getUsers(request):
     parser = DataParser()
     parser.parseFile("Participant-attribute-data.csv", True)
-    response = parser.getQuestions()
+    response = parser.getResponses()
     for val in response:
-        thing = json.loads(val)
         newU = User()
-        newU.userID = thing['id']
-        newU.gender = thing['gender']
-        newU.age = thing['age']
-        newU.diagnosis = thing['diagnosis']
-        newU.income = thing['income']
-        newU.education = thing['education']
-        newU.ethnicity = thing['ethnicity']
-        newU.password = thing['password']
-        newU.email = thing['email']
+        newU.userID = val[0]
+        newU.gender = val[1]
+        newU.age = val[2]
+        newU.diagnosis = val[3]
+        newU.income = val[4]
+        newU.education = val[5]
+        newU.ethnicity = val[6]
+        newU.password = val[7]
+        newU.email = val[8]
         newU.save()
 
     return HttpResponse("okaaaaaaaaaaa!")
