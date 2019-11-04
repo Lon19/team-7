590f3c858dd81673553aad3d1181f6cef12192f9
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2019-11-04 00:12:00 +0000

    Commenting on urls file

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,13 +1,16 @@
-# visualisations URL Configuration
+# user URL Configuration
 
 from django.urls import path
 
 from . import views
 
+""" This urls file determines each of the path ends for the /user section,
+    along with their views functions"""
+
 urlpatterns = [
     path('', views.index, name='index'), # index pages
-    path('users', views.getUsers, name='users'),
-    path('answers', views.getAnswers, name='answers'),
+    path('users', views.getUsers, name='users'), # used to save users, should be automatic
+    path('answers', views.getAnswers, name='answers'), # used to add answers, should also be automatic
     path('respondent/home', views.user_home, name='user_home'),
     path('respondent/<int:user_id>/questionnaire/<int:questionnaire_id>', views.user_questionnaire, name='questionnaire'),
     path('respondent/<int:user_id>/visualisation/<int:questionnaire_id>', views.user_questionnaire_responses, name='questionnaire-responses'),
