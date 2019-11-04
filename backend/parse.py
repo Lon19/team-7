58090dcb8c980560fbebe9bc3a07a9b79b430f3e
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2019-11-04 00:12:19 +0000

    Comments on parse file

--- backend/parse.py
+++ backend/parse.py
@@ -2,15 +2,20 @@ import csv
 import json
 from user.models import QuestionType, AnswerType
 
-class DataParser():
+""" DataParser class is an API created with the purpose of decyphering the CSV
+    files given to us from WordPress, to then send to the webserver to add to the database"""
 
+# Note: This was initially fully using JSON, however python was handling it weirdly,
+# So there was a last minute switch to arrays, despite this being bad practice
+class DataParser():
 
     def __init__(self):
         self.responses = []
         self.questions = []
         self.isUsers = False
 
-
+    """ parseFile opens the requested CSV file to return the questions and
+        their responses, storing them into the respective arrays """
     def parseFile(self, fileName, isUsers):
         self.isUsers = isUsers
         with open(fileName,'rt')as f:
@@ -23,6 +28,8 @@ class DataParser():
                     self.responses.append(row)
                 count += 1
 
+    """ determineQType is a (fairly rough) algorithm designed to figure out if
+        the question is multiple choice or not, and if so, what kind of question it is"""
     def determineQType(self, index):
         options = []
         for record in self.responses:
@@ -31,6 +38,8 @@ class DataParser():
                 options.append(value)
         options.sort()
         name = ""
+        # There needs to be a check to see if there are "too many" options
+        # If so, then it must be an open ended question.
         for item in options:
             name += item[:9]
         #print(name)
@@ -40,22 +49,29 @@ class DataParser():
                 return counter
             counter += 1
         else:
-            print("Ok I'm here")
-            qType = QuestionType(questionTypeName=name)# TODO ADD ANSWER TYPES HERE
+            #print("Ok I'm here")
+            qType = QuestionType(questionTypeName=name)
             qType.save()
+            # Algorithm to add answer types. Could also be improved with
+            # table.models.add(records)
             for item in options:
                 aType = AnswerType(answerTypeID=len(AnswerType.objects.all())+1)
                 aType.questionTypeID = qType
                 aType.textRef = item
                 aType.progRef = len(AnswerType.objects.all())+1
                 aType.save()
-
             return counter
 
+    """ getQuestions searches the first line of the CSV to get each of the
+        questions in the questionnaire, determining their types initially.
+        This is then all added to the database.
+        This works differently if isUsers is True"""
     def getQuestions(self):
         questionResponse = []
 
         if not self.isUsers:
+            # These values only work if each CSV is the same, which can't be guaranteed
+            # So would ideally be dynamically checked in code
             questionResponse.append(json.dumps({'FormName': self.responses[0][0]}))
             questionResponse.append(json.dumps({'username': self.questions[2]}))
             questionResponse.append(json.dumps({'iD': self.questions[-2]}))
@@ -64,26 +80,25 @@ class DataParser():
             for i in range(3,len(self.questions)-2):
                 questionJSON = []
                 questionJSON.append(self.questions[i])
-                questionJSON.append(self.determineQType(i)) # Hard coded for now
+                questionJSON.append(self.determineQType(i))
                 questionResponse.append(questionJSON)
         else:
-            return # Don't need it for users
-
-
+            return # Don't need it for users, at least for hackathon
         return questionResponse
 
-
+    """ getResponses searches each of the other lines of the CSV to create a list
+        of responses, containing the ID, date and answers. Or in the users case,
+        it simply returns each of the user information values"""
     def getResponses(self):
         responses = []
         if not self.isUsers:
             for record in self.responses:
-                # Only count from 3 to -3
+                # Ended
                 responses.append(record)
-
-
             return responses
         else:
             for record in self.responses:
+                # Password values need to be hashed
                 questionJSON = []
                 questionJSON.append(record[0])
                 questionJSON.append(record[1])
@@ -98,6 +113,7 @@ class DataParser():
         return responses
 
 
-data = DataParser()
-data.parseFile("wpforms-Autistica-8211-Mental-Health.csv",False)
-data.getQuestions()
+# data = DataParser()
+# data.parseFile("wpforms-Autistica-8211-Mental-Health.csv",False)
+# data.getQuestions()
+# This was test code, used to test away from the server
